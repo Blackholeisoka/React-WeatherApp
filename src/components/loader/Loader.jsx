b829e70f84368f037f0b873dc9b565878a5b0436
Blackholeisoka <blackholeisoka@gmail.com>
2025-06-06 22:10:41 +0200

    little modif

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -5,7 +5,7 @@ function Loader() {
 
   return (
     <div className='fade-in-up' style={{textAlign : 'center', color : '#fff'}}>
-      <img style={{width: '100%'}} src={LoaderImg} alt="" />
+      <img style={{width: '344px'}} src={LoaderImg} alt="" />
       <h1 className='loader_title' style={{fontSize : '40px', transform : 'translateY(-100px)', marginBottom : '10px'}}>Loading Weather</h1>
       <p style={{transform : 'translateY(-100px)'}}>Please wait while we fetch the latest forecast.</p>
     </div>
--- src/components/primary/CitationRandom.jsx
+++ src/components/primary/CitationRandom.jsx
@@ -7,10 +7,18 @@ function CitationRandom() {
   useEffect(() => {
     const getCitation = async () => {
       try {
-        const url = 'http://api.quotable.io/random?maxLength=70';
-        const response = await fetch(url);
-        const result = await response.json();
-        setCitationData({ body: result.content, author: result.author });
+        const url = encodeURIComponent('https://zenquotes.io/api/random');
+        const response = await fetch(`https://api.allorigins.win/get?url=${url}`);
+        if (!response.ok) throw new Error('Réponse réseau non valide');
+
+        const data = await response.json();
+        // 'contents' est une chaîne JSON, il faut la parser
+        const result = JSON.parse(data.contents);
+
+        setCitationData({
+          body: result[0].q,
+          author: result[0].a,
+        });
       } catch (error) {
         console.error('Erreur lors du fetch:', error);
       }
@@ -18,7 +26,6 @@ function CitationRandom() {
 
     getCitation();
   }, []);
-
 
   if (!citationData) {
     return <div>Chargement de la citation...</div>;
@@ -33,7 +40,7 @@ function CitationRandom() {
       <div className="citation_text">
         <blockquote>
           "{citationData.body}"
-          <footer>- <cite style={{fontStyle : 'normal', fontWeight : 'normal'}}>{citationData.author}</cite></footer>
+          <footer>- <cite style={{ fontStyle: 'normal', fontWeight: 'normal' }}>{citationData.author}</cite></footer>
         </blockquote>
       </div>
     </div>
